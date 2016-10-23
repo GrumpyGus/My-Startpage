9ecefd59609fc9be8944907e4b7b87563b808524
Mac Merpaw <mmerp@hotmail.com>
2016-10-22 21:56:34 -0400

    Only create date formatter  object once.

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,10 @@
 ﻿document.addEventListener("DOMContentLoaded", function(event) {
-    setTime('bind-header-clock'); 
-    window.setInterval(function () {setTime('bind-header-clock')}, 1000);
+    var formatter = new dateFormatter();
+    setTime('bind-header-clock', formatter); 
+    window.setInterval(function () {setTime('bind-header-clock', formatter)}, 1000);
 });
 
 
-function setTime(elementId) {
-    var formatter = new dateFormatter();
-    document.getElementById(elementId).innerHTML = formatter.formatDate(new Date());
+function setTime(elementId, dateFormatter) {
+    document.getElementById(elementId).innerHTML = dateFormatter.formatDate(new Date());
 }
